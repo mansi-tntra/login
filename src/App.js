9005eaa844cec17ea58bd4e1617fba0b56edc78d
mansi-tntra <mansi.kachhiya@tntra.io>
2022-11-08 19:27:21 +0530

    private route

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import Login from "./Login";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {routeConfig} from "./routeConfig";
+import {PrivateRoute} from "./privateRoute";
+import loginPage from "./LoginPage";
 
 function App() {
   return (
     <div className="App">
     <BrowserRouter>
         <Routes>
+            <Route exact path='/' element={<PrivateRoute/>}/>
             <Route path={routeConfig[0].path} element={routeConfig[0].component}/>
             <Route path={routeConfig[1].path} element={routeConfig[1].component}/>
             <Route path={routeConfig[2].path} element={routeConfig[2].component}/>
