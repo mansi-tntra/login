d17975154bea0e9e35ade6a65272693292c57307
mansi-tntra <mansi.kachhiya@tntra.io>
2022-11-08 19:01:18 +0530

    REDUX ADD

--- src/Home /index.js
+++ src/Home /index.js
@@ -2,12 +2,15 @@ import {Button} from "antd";
 import {logout} from "../Login/reducer";
 import {useDispatch, useSelector} from "react-redux";
 import {NavLink} from "react-router-dom";
+import Error from "../Error/ErrorComponent";
+import React from "react";
 
  const Home = ()=>{
-
+     const { message } = useSelector(state => state.auth);
     return(
       <>
           <h1> Home page </h1>
+          {message && <Error>{message}</Error>}
       </>
 
     )
